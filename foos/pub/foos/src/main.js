872daad31b69ad2e9ce7baf2906255346a206ec3
Galen Wolfe-Pauly <root@office-for.com>
2015-03-17 18:34:39 -0700

    further foos

--- foos/pub/foos/src/main.js
+++ foos/pub/foos/src/main.js
@@ -1,6 +1,8 @@
 div = React.DOM.div
 ul  = React.DOM.ul
 li  = React.DOM.li
+input  = React.DOM.input
+button = React.DOM.button
 
 recl = React.createClass
 
@@ -8,11 +10,106 @@ recl = React.createClass
 Page = recl({
   render: function(){return (
     div({},
-    //AddPlayer({players: this.props.players}),
-    Fixtures({fixturesList:this.props.fixturesList}), //don't forget to include the key before the ':' here. 
+    AddFixture({fixtures:this.props.fixture}),
+    Fixtures({fixturesList:this.props.fixturesList}), // don't forget to include the key before the ':' here. 
     Standings({fixturesList:this.props.fixturesList})
     )
   )},
+})
+
+AddFixture = recl({
+  _handleClick: function() { submit(); },
+  _handleKey: function(e) { if(e.keyCode == 13) submit(); },
+  submit: function() {
+    $el = $(this.getDOMNode())
+
+    bcons = $el.find('#bcons').val().trim()
+    bscore = parseInt($el.find('#bscore').val().trim())
+
+    ycons = $el.find('#bcons').val().trim()
+    yscore = parseInt($el.find('#yscore').val().trim())
+
+    var bscr = parseInt(bscore.value)
+    var yscr = parseInt(yscore.value)
+
+    if(bcons.length === 0 || 
+       ycons.length === 0 || 
+       isNaN(bscore) || 
+       isNaN(yscore)) { 
+      alert("Something isn't quite right with your input"); 
+      return; 
+    }
+    valid = this.isValid(bscore,yscore,bcons,ycons)
+    if(valid !== true){      // check for valid scoreline
+      urb.send({
+        appl: "foos",
+        data: {
+          bcons:bcons, 
+          ycons:ycons, 
+          bscore:bscore, 
+          yscore:yscore
+        },
+        mark: "json"
+      })
+      newFixturesList = mounted.props.fixturesList.slice();
+      newFixturesList.push({ // dont forget to send object
+        bcons:bcons,
+        ycons:ycons, 
+        bscore:bscore, 
+        yscore:yscore,
+        pending:true
+      })
+      mounted.setProps({fixturesList:newFixturesList})
+    } else { 
+      alert(valid); 
+    }
+  },
+
+  isValid: function(bscore,yscore,bcons,ycons){
+    valid = true
+    if(this.validRange(bscore) && this.validRange(yscore)){
+      if(bscore == 10 || yscore == 10){
+        if(yscore == 10 && bscore == 10) valid = "Scores can't both be 10?"
+      } else valid = "Scores aren't quite right"
+    } else valid = "Scores aren't quite right"
+    if(bcons.toLowerCase() == ycons.toLowerCase())
+      valid = "Two players can't be the same!"
+    return valid
+  },
+
+  validRange: function(score){
+    if(score < 0 || score > 10) return false
+    return true
+  },
+
+  render: function() {
+    return div({id:"afix"},
+      [div({className:'contestant'},'Contestant'),
+       div({className:'score'},'Score'),
+       div({className:'score'},'Score'),
+       div({className:'contestant'},'Contestant'),
+       input({
+        className:'contestant black', 
+        id:'bcons', 
+        onKeyDown:this._handleKey}),
+       input({
+        className:'score black', 
+        id:'bscore', 
+        onKeyDown:this._handleKey}),
+       input({
+        className:'score yellow', 
+        id:'yscore', 
+        onKeyDown:this._handleKey}),
+       input({
+        className:'contestant yellow', 
+        id:'ycons', 
+        onKeyDown:this._handleKey}),
+       button({
+        id:'entrees', 
+        onClick:this._handleClick}, 
+        'Submit')
+    ])
+  }
 })
 
 //List all played fixtures
@@ -24,11 +121,13 @@ Fixtures = recl({
 
 Fixture = recl({
   render: function() {
-    return li({className:'fixture'}, 
-            [div({className:'contestant'}, fix.bcons), ' ',
-             div({className:'score'}, fix.bscore), '-', 
-             div({className:'score'}, fix.yscore), ' ', 
-             div({className:'contestant'}, fix.ycons)])}
+    klass = 'fixture'
+    if(this.props.pending) klass += ' pending'
+    return li({className:klass}, 
+            [div({className:'contestant'}, this.props.bcons),
+             div({className:'score'}, this.props.bscore),
+             div({className:'score'}, this.props.yscore),
+             div({className:'contestant'}, this.props.ycons)])
   }
 })
 
@@ -88,3 +187,13 @@ fixturesToStandings = function (fixtures) {
   })
 }
 
+$(document).ready(function() {
+  mounted = React.render(Page({players:[], fixturesList:[]}), $("#container")[0])
+  urb.subscribe({
+    appl: "foos",
+    path: "/"
+  }, function(err,d){
+    if(d.data.ok) return;
+    mounted.setProps({fixturesList:d.data})
+  })
+})
